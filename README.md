cec30062556ba255cfa161021eaa5dbbf2ba97eb
RMElam <34106042+RMElam@users.noreply.github.com>
2018-06-27 00:27:02 -0400

    added timer, star ratings to game and to the congrats modal. Updated READme file and fixed all other corrections listed in project review

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
 # Memory Game Project
 
+A game where you try to match all of the cards as fast as you can. This games HTML and CSS was written out and provided by Udacity. 
+
+Known bugs: The congrats modal will not display the players rating.
+
 ## Table of Contents
 
-* [Instructions](#instructions)
 * [Contributing](#contributing)
-
-## Instructions
-
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+* [Author](#author)
+* [Acknowledgements](#acknowledgements)
 
 ## Contributing
 
@@ -19,3 +16,13 @@ This repository is the starter code for _all_ Udacity students. Therefore, we mo
 
 For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
 # fend-project-memory-game
+
+##Author
+
+Rebecca Elam
+
+##Acknowledgements
+
+* Udacity Mentor: Leyth 
+* Udacity Course 
+* Mike Whales' live webinar
--- js/app.js
+++ js/app.js
@@ -8,18 +8,22 @@ let cards = ["fa-diamond", "fa-diamond",
 "fa-bicycle", "fa-bicycle", 
 "fa-bomb", "fa-bomb"];
 
-let card = document.getElementsByClassName("card");
+const card = document.getElementsByClassName("card");
 
 //open cards array
 let openCards = [];
 
 //matched cards
-let matchedCards = document.getElementsByClassName('match');
+const matchedCards = document.getElementsByClassName('match');
 
 
 //moves counter
 let moves = 0
 let counter = document.querySelector('.moves');
+
+//rating stars
+let starsList = document.querySelectorAll(".stars li");
+const stars = document.querySelectorAll('.fa-star');
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -58,15 +62,25 @@ function startGame(){
 	//restart moves counter
 	let moves = 0
 	counter.innerHTML = moves;
-};
-startGame();
-playGame();
+
+	 //reset timer
+	 let second = 0;
+	 let minute = 0; 
+	 let hour = 0;
+	 let interval;
+	 const timer = document.querySelector(".timer");
+	 timer.innerHTML = "0 mins 0 secs";
+	 clearInterval(interval);
+	};
+	startGame();
+	playGame();
 
  //turn cards over/push cards into an array/ test if cards match 
  function playGame() {
  	for (let i=0; i<cards.length; i++) {
  		card[i].addEventListener('click', function() {
  			this.classList.add('open', 'show', 'disabled');
+ 			
 
  			if (this.classList.contains('open') || this.classList.contains('show') || this.classList.contains('disabled')) {
  				openCards.push(this);
@@ -123,6 +137,46 @@ function unmatched() {
 function moveCounter(){
 	moves++;
 	counter.innerHTML = moves;
+	//start timer
+	if (moves == 1){
+		startTimer();
+	}
+
+	//star ratings
+	if(moves > 10 && moves < 20){
+		for( i = 0; i < 3; i++) {
+			if(i > 1){
+				stars[i].style.visibility = "collapse";
+			}
+		}
+	}else if (moves > 21){
+		for(i = 0; i < 3; i++){
+			if(i > 0){
+				stars[i].style.visibility = "collapse";
+			}
+		}
+	}
+};
+
+//game timer
+const timer = document.querySelector('.timer');
+let second = 0;
+let minute = 0;
+let hour = 0;
+let interval;
+function startTimer(){
+	interval = setInterval(function(){
+		timer.innerHTML = minute+'mins '+second+'secs';
+		second++;
+		if(second === 60){
+			minute++;
+			second = 0;
+		}
+		if(minute === 60){
+			hour++;
+			minute = 0;
+		}
+	}, 1000);
 };
 
 //restart button
@@ -139,10 +193,17 @@ restartButton();
 //congrats modal
 function congratsModal(){
 	let totalCount = counter.innerHTML;
+	let starRating = stars.innerHTML;
+	let totalTimer = timer.innerHTML;
 	if (matchedCards.length == 16) {
+		clearInterval(interval);
 		swal({
 			title: "Congratulations!",
-			text: `"You did it in ${totalCount} moves!"`
+			text: `You did it in ${totalCount} moves!
+			Your rating is ${starRating} stars!
+			Your total time was ${totalTimer}. 
+			Want to play again?`
+			
 		})
 	}
 };
@@ -151,7 +212,3 @@ function congratsModal(){
 for(let i=0; i<cards.length; i++) {
 	card[i].addEventListener('click', congratsModal);
 };
-
-
-
-
